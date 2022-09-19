c0299e9677ab30a44a8c8df89eab302bff32fe35
EkamjotKaur19 <106084982+EkamjotKaur19@users.noreply.github.com>
2022-09-20 00:12:12 +0530

    Update magic_sq.cpp

--- tictactoe/magic_sq.cpp
+++ tictactoe/magic_sq.cpp
@@ -1,96 +1,97 @@
 #include<iostream>
 #include<bits/stdc++.h>
-#include<stdio.h>
-#include<windows.h>
+
 using namespace std;
 
+
 int board[10]={2,2,2,2,2,2,2,2,2,2};
-int turn=1;
-int flag=0;
-int player, comp;
-void comp_game();
-void user_game();
-void draw_cond();
-void board_state();
+int turn=0;
 
-bool checkWin(int player){
-    vector<int>    mp = {0, 8, 3, 4, 1, 5, 9, 6, 7, 2};
-    vector<int> revmp = {0, 4, 9, 2, 3, 5, 7, 8, 1, 6};
+bool checkWin(){
+    int mp[10] = {0, 8, 3, 4, 1, 5, 9, 6, 7, 2};
+    int revmp[10] = {0, 4, 9, 2, 3, 5, 7, 8, 1, 6};
 
-    vector<int>comp_moves;
-    vector<int>user_moves;
+    vector<int> comp_moves, user_moves;
 
-    for(int i=1; i<10; i++){
-        if(board[i]==3){
-            user_moves.push_back(mp[i]);
-        }
-
-        if(board[i]==5){
+    for (int i = 1; i <= 9; i++){
+        if (board[i] == 5){
             comp_moves.push_back(mp[i]);
         }
-    }
-    if(player==1){
-        for(int i=0; i<comp_moves.size(); i++){
-            for(int j=i+1; j<comp_moves.size(); j++){
-                for(int k=j+1; k<comp_moves.size(); k++){
-                    if(comp_moves[i]+comp_moves[j]+comp_moves[k]==15){
-                        return true;
-                    }
-                    else return false;
-                }
-            }
+        else if (board[i] == 3){
+            user_moves.push_back(mp[i]);
         }
     }
-    if(player==2){
-        for(int i=0; i<user_moves.size(); i++){
-            for(int j=i+1; j<user_moves.size(); j++){
-                for(int k=j+1; k<user_moves.size(); k++){
-                    if(user_moves[i]+user_moves[j]+user_moves[k]==15){
-                        return true;
-                    }
-                    else return false;
+
+    for (int i = 0; i < comp_moves.size(); i++){
+        for (int j = i+1; j < comp_moves.size(); j++){
+            for (int k = j+1; k < comp_moves.size(); k++){
+                int x = 15-comp_moves[i]-comp_moves[j]-comp_moves[k];
+                if (x == 0){
+                    return true;
                 }
             }
         }
     }
 
-    
-    return false;
+    for (int i = 0; i < user_moves.size(); i++){
+        for (int j = i+1; j < user_moves.size(); j++){
+            for (int k = j+1; k < user_moves.size(); k++){
+                int x = 15-user_moves[i]-user_moves[j]-user_moves[k];
+                if (x == 0){
+                    return true;
+                }
+            }
+        }
+    }
+    return false;  
 }
 
-
-void gotoxy(short a, short b){
-    COORD coordintes;
-    coordintes.X=a;
-    coordintes.Y=b;
-
-    SetConsoleCursorPosition(
-        GetStdHandle(STD_OUTPUT_HANDLE), coordintes
-    );
-
+char put_X_O(int n){
+    if(n==3){
+        return 'X';
+    }
+    if(n==5){
+        return 'O';
+    }
+    else{
+        return ' ';
+    }
 }
 
-//First function - Make 2
-int make_two(){
-    if(board[5]==2){
-        return 5;
+void display(){
+    for(int i=1; i<=3; i++){
+        cout<<put_X_O(board[i])<<" ";
+        if(i!=3){
+            cout<<" | ";
+        }
     }
-    else if(board[2]==2){
-        return 2;
+    cout<<endl;
+    cout<<"----------------"<<endl;
+
+    for(int i=4; i<=6; i++){
+        cout<<put_X_O(board[i])<<" ";
+        if(i!=6){
+            cout<<" | ";
+        }
     }
-    else if(board[4]==2){
-        return 4;
+    cout<<endl;
+    cout<<"----------------"<<endl;
+
+    for(int i=7; i<=9; i++){
+        cout<<put_X_O(board[i])<<" ";
+        if(i!=9){
+            cout<<" | ";
+        }
     }
-    else if(board[6]==2){
-        return 6;
-    }
-    else if(board[8]==2){
-        return 8;
-    }
-    return 0;
+    cout<<endl;
+    cout<<endl;
+    cout<<endl;
+    cout<<endl;
+    cout<<endl;
+    cout<<endl;
+    cout<<endl;
 }
 
-//Second Function - Go(n)
 void Go(int n){
     if(turn%2!=0){
         board[n]=3;
@@ -101,62 +102,61 @@ void Go(int n){
     turn++;
 }
 
-int Posswin(int turn){
-    vector<int> m_sq = {0, 8, 3, 4, 1, 5, 9, 6, 7, 2};
-    vector<int> rev = {0, 4, 9, 2, 3, 5, 7, 8, 1, 6};
+int make_two()
+{
+    int pos;
 
-    if (turn%2 == 0){
-        vector<int> comp_moves;
-        vector<int> user_moves;
+    if (board[5] == 2)
+    {
+        pos = 5;
+    }
+    else
+    {
+        bool done = false;
 
-        for (int i = 1; i <= 9; i++){
-            if (board[i] == 5){
-                comp_moves.push_back(m_sq[i]);
-            }
-            else if (board[i] == 3){
-                user_moves.push_back(m_sq[i]);
-            }
+        for (int i = 2; i <= 8; i += 2)
+        {
+            if (board[i] == 2) {pos = i; done = true; break;}
         }
 
-        for (int i = 0; i < comp_moves.size(); i++){
-            for (int j = i+1; j < comp_moves.size(); j++){
-                int x = 15-comp_moves[i]-comp_moves[j];
-                if (x > 0 && x < 10 && board[rev[x]] == 2){
-                    return rev[x];
-                }
-            }
-        }
-
-        for (int i = 0; i < user_moves.size(); i++){
-            for (int j = i+1; j < user_moves.size(); j++){
-                int x = 15-user_moves[i]-user_moves[j];
-                if (x > 0 && board[rev[x]] == 2){
-                    return rev[x];
-                }
-            }
+        if (!done)
+        {
+            for (int i = 1; i <= 9; i += 2)
+            {
+                if (board[i] == 2) {pos = i; break;}
+            }            
         }
     }
 
-    else{
-        vector<int> comp_moves;
-        vector<int> user_moves;
+    return pos;
+}
+
+int Poswin(int turn){
+    int mp[10] = {0, 8, 3, 4, 1, 5, 9, 6, 7, 2};
+    int revmp[10] = {0, 4, 9, 2, 3, 5, 7, 8, 1, 6};
+
+    if (turn%2 == 0){
+        vector<int> comp_moves, user_moves;
 
         for (int i = 1; i <= 9; i++){
-            if (board[i] == 3){
-                comp_moves.push_back(m_sq[i]);
+            if (board[i] == 5)
+            {
+                comp_moves.push_back(mp[i]);
             }
-            else if (board[i] == 5){
-                user_moves.push_back(m_sq[i]);
+            else if (board[i] == 3)
+            {
+                user_moves.push_back(mp[i]);
             }
         }
 
-        for (int i = 0; i < comp_moves.size(); i++){
+        for (int i = 0; i < comp_moves.size(); i++)
+        {
             for (int j = i+1; j < comp_moves.size(); j++)
             {
                 int x = 15-comp_moves[i]-comp_moves[j];
-                if (x > 0 && x < 10 && board[rev[x]] == 2)
+                if (x > 0 && x < 10 && board[revmp[x]] == 2)
                 {
-                    return rev[x];
+                    return revmp[x];
                 }
             }
         }
@@ -166,149 +166,243 @@ int Posswin(int turn){
             for (int j = i+1; j < user_moves.size(); j++)
             {
                 int x = 15-user_moves[i]-user_moves[j];
-                if (x > 0 && board[rev[x]] == 2)
+                if (x > 0 && board[revmp[x]] == 2)
                 {
-                    return rev[x];
+                    return revmp[x];
+                }
+            }
+        }
+    }
+    else
+    {
+        vector<int> comp_moves, user_moves;
+
+        for (int i = 1; i <= 9; i++)
+        {
+            if (board[i] == 3)
+            {
+                comp_moves.push_back(mp[i]);
+            }
+            else if (board[i] == 5)
+            {
+                user_moves.push_back(mp[i]);
+            }
+        }
+
+        for (int i = 0; i < comp_moves.size(); i++)
+        {
+            for (int j = i+1; j < comp_moves.size(); j++)
+            {
+                int x = 15-comp_moves[i]-comp_moves[j];
+                if (x > 0 && x < 10 && board[revmp[x]] == 2)
+                {
+                    return revmp[x];
+                }
+            }
+        }
+
+        for (int i = 0; i < user_moves.size(); i++)
+        {
+            for (int j = i+1; j < user_moves.size(); j++)
+            {
+                int x = 15-user_moves[i]-user_moves[j];
+                if (x > 0 && board[revmp[x]] == 2)
+                {
+                    return revmp[x];
                 }
             }
         }
     }
 
     return -1;
-
-
 }
 
-void comp_game(){
-    if(Posswin(turn)){
-        Go(Posswin(turn));
-        flag=1;
+int main()
+{
+    
+    cout<<"Tic Tac Toe - The Game\n";
+    cout<<"First turn or Second Turn?\nEnter 1 for first turn, 2 for second turn\n";
+    char x;
+    int turn;
+    if (x == '1'){turn = 2;} else {turn = 1;}
+
+
+    vector<int> TMP;
+    display();
+    if (turn == 2)
+    {
+        cout<<"Input position: \n";
+        int x; char C;
+        x = C - 48;
+        Go(x);
+        display();
     }
-    else{
-        if(Posswin(turn)){
-            Go(Posswin(turn));
+
+    while (turn <= 9)
+    {
+        cout<<"Computer is thinking....\n";
+        switch (turn)
+        {
+            case 1:
+                Go(1);
+                break;
+
+            case 2:
+                if (board[5] == 2)
+                {
+                    Go(5);                
+                }
+                else
+                {
+                    Go(1);
+                }
+                break;
+
+            case 3:
+                if (board[9] == 2)
+                {
+                    Go(9);                  
+                }
+                else
+                {
+                    Go(3);
+                }
+                break;
+
+            case 4:
+                if (Poswin(turn) != -1)
+                {
+                    Go(Poswin(turn));
+                }
+                else
+                {
+                    if (( board[1] == 3 || board[4] == 3 ) && board[8] == 3)
+                    {
+                        Go(9);
+                    }
+                    else if (( board[3] == 3 || board[6] == 3 ) && board[8] == 3)
+                    {
+                        Go(7);
+                    }
+                    else if (board[5] == 3 && board[9] == 3)
+                    {
+                        Go(7);
+                    }
+                    else if (( board[4] == 3 || board[6] == 3 ) && board[2] == 3)
+                    {
+                        Go(1);
+                    }
+                    else
+                    {
+                        Go(make_two());
+                    }
+                }
+                break;
+
+            case 5:
+                if (Poswin(turn) != -1)
+                {
+                    Go(Poswin(turn));
+                }
+                else
+                {
+                    if (board[7] == 2)
+                    {
+                        Go(7);
+                    }
+                    else
+                    {
+                        Go(3);
+                    }
+                }
+                break;
+
+            case 6:
+                if (Poswin(turn) != -1)
+                {
+                    Go(Poswin(turn));
+                }
+                else
+                {
+                    Go(make_two());
+                }
+                break;
+           
+            case 7:
+                if (Poswin(turn) != -1)
+                {
+                    Go(Poswin(turn));
+                }
+                else
+                {
+                    Go(make_two());
+                }
+                break;
+
+            case 8:
+                if (Poswin(turn) != -1)
+                {
+                    Go(Poswin(turn));
+                }
+                else
+                {
+                    Go(make_two());
+                }
+                break;
+
+            case 9:
+                if (Poswin(turn) != -1)
+                {
+                    Go(Poswin(turn));
+                }
+                else
+                {
+                    Go(make_two());
+                }  
+                break;            
         }
-        else{
-            Go(make_two());
+
+       system("cls");
+        display();
+
+        if (checkWin())
+        {
+            cout<<"\nComputer Won!\n";
+            break;
         }
 
-    }
-    board_state();
+        if (turn != 9)
+        {
+            cout<<"Enter your move: ";
+            int x;
+            char C;
+            x = C - 48;
 
-    if(flag){
-        gotoxy(30,20);
-        cout<<"Computer Wins"<<endl;
-        return;
-    }
-    else{
-        user_game();
-    }
-}
+            while (board[x] != 2)
+            {
+                cout<<"Enter valid position ; ";
+                cout<<"\n";
+                x = C-48;
+            }
+            Go(x);
+           system("cls");
+            display();
 
-void user_game(){
-    int pos;
-    int player=1;
-    if(checkWin(player)){
-        return;
-    }
-    board_state();
-    gotoxy(30,18);
-    cout<<"Your move at position: "<<endl;
-    cin>>pos;
-    if(board[pos]!=2){
-        cout<<"Already occupied position"<<endl;
-        user_game();
-    }
-    if(pos == Posswin(turn)){
-        Go(pos);
-        board_state();
-        gotoxy(30,20);
-        cout<<"Player wins"<<endl;
-        return;
-    }
-    Go(pos);
-    board_state();
-    comp_game();
-}
-
-void draw_cond(){
-    if(turn>9){
-        cout<<"Game Draw"<<endl;
-    }
-}
-
-void put_X_O(char ch, int pos){
-    int m;
-    int x=31, y=10;
-    m=pos;
-    if(m>3){
-        while(m>3){
-            y=y+3;
-            m=m-3;
-        }
-    }
-
-    if(pos%3 == 0){
-        x=x+16;
-    }
-    else{
-        pos = pos%3;
-        pos--;
-        while(pos){
-            x=x+8;
-            pos--;
-        }
-    }
-    gotoxy(x,y);
-    cout<<ch<<endl;
-}
-
-void board_state(){
-    int j;
-    for(j=9; j<17; j++){
-        gotoxy(35,j);
-        cout<<"|          |"<<endl;
-    }
-    gotoxy(28,11);
-    cout<<"--------------------------"<<endl;
-
-    gotoxy(28,14);
-    cout<<"--------------------------"<<endl;
-
-    for(j=1; j<10; j++){
-        if(board[j]==3){
-            put_X_O('X', j);
-        }
-        else{
-            if(board[j]==5){
-                put_X_O('0', j);
+            if (checkWin())
+            {
+                cout<<"\nYou Won!\n";
+                break;
             }
         }
+
+        turn+=2;
     }
-}
 
+    if (turn > 9)
+    {
+        cout<<"Game Draw!\n";
+    }
 
-
-int main(){
     
-    int choice;
-
-    cout<<"Do You Want To Play?"<<endl;
-    cout<<"1. Yes"<<endl;
-    cout<<"2. No"<<endl;
-    cin>>choice;
-    turn=1;
-    switch(choice){
-        case 1:
-        player=1;
-        comp=0;
-        user_game();
-        break;
-
-        case 2:
-        comp=1;
-        player=0;
-        comp_game();
-        break;
-    }
+    system("cls");
 }
