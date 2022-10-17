bde1d35c3d790c39ea81182ed8650d4bf0051669
EkamjotKaur19 <106084982+EkamjotKaur19@users.noreply.github.com>
2022-10-17 23:15:43 +0530

    Update minimax.cpp

--- tictactoe/minimax.cpp
+++ tictactoe/minimax.cpp
@@ -1,27 +1,9 @@
-/*Minimax Algorithm*/
-#include<iostream>
 #include<bits/stdc++.h>
-#include<stdio.h>
-#include<windows.h>
 using namespace std;
 
-//Steps
-/*
-1. Initialized empty board
-2. Put OX
-3. Display func for board
-4. Go function
-5. make two function
-5. Posswin
-
-*/
-
-vector<int>board(10,2);
-int turn=0;
-int flag=0;
-int dep=1;
-int player, comp;
-void user_game();
+int d=9;
+bool win=0;
+int winner;
 
 char put_X_O(int n){
     if(n==3){
@@ -35,7 +17,7 @@ char put_X_O(int n){
     }
 }
 
-void display(){
+void display(vector<int>board){
     for(int i=1; i<=3; i++){
         cout<<put_X_O(board[i])<<" ";
         if(i!=3){
@@ -43,7 +25,7 @@ void display(){
         }
     }
     cout<<endl;
-    cout<<"-------------------"<<endl;
+    cout<<"----------------"<<endl;
 
     for(int i=4; i<=6; i++){
         cout<<put_X_O(board[i])<<" ";
@@ -52,7 +34,7 @@ void display(){
         }
     }
     cout<<endl;
-    cout<<"-------------------"<<endl;
+    cout<<"----------------"<<endl;
 
     for(int i=7; i<=9; i++){
         cout<<put_X_O(board[i])<<" ";
@@ -64,141 +46,149 @@ void display(){
     cout<<endl;
     cout<<endl;
     cout<<endl;
-    cout<<endl;
-    cout<<endl;
-    cout<<endl;
 }
 
-void Go(int n){
-    if(turn%2!=0){
-        board[n]=3;
-    }
-    else{
-        board[n]=5;
-    }
-    turn++;
-}
-
-vector<vector<int>>move_gen(){
-    vector<vector<int>>ans;
-    for(int i=0; i<9; i++){
-        if(board[i]==2){
-            board[i]=3;
-            ans.push_back(board);
-            board[i]=2;
+int updatewin(vector<int>board){
+    for(int i=0;i<3;i++){
+        if(board[i*3+1]*board[i*3+2]*board[i*3+3]==27){
+            winner=1;
+            return 1;
         }
-    }
-    return ans;
-}
-
-int evaluate(int player){
-    int comp_third, comp_sec, comp_first;
-    int opp_third, opp_sec, opp_first;
-
-    if(player==0){
-        comp_third=27;
-        comp_sec=18;
-        comp_first=12;
-    }
-    else{
-        opp_third=125;
-        opp_sec=50;
-        opp_first=20;
-    }
-
-    for(int i=1;i<=9; i=i+3){
-        if(board[i]*board[i+1]*board[i+2]==comp_third){
-            return 10;
+        else if(board[i*3+1]*board[i*3+2]*board[i*3+3]==125){
+            winner=2;
+            return -1;
         }
-        if(board[i]*board[i+1]*board[i+2]==opp_third){
-            return -10;
+        if(board[1+i]*board[4+i]*board[7+i]==27){
+            winner=1;
+            return 1;
         }
-    }
-
-    for(int i=1;i<=9; i=i++){
-        if(board[i]*board[i+3]*board[i+6]==comp_third){
-            return 10;
+        else if(board[1+i]*board[4+i]*board[7+i]==125){
+            winner=2;
+            return -1;
         }
-        if(board[i]*board[i+3]*board[i+6]==opp_third){
-            return -10;
+        if(board[1]*board[5]*board[9]==27){
+            winner=1;
+            return 1;
         }
-    }
-
-    if(board[1]*board[5]*board[9] == comp_third){
-        return 10;
-    }
-    if(board[3]*board[5]*board[7] == opp_third){
-        return -10
-    }
-
-
-    for(int i=1;i<=9; i=i+3){
-        if(board[i]*board[i+1]*board[i+2]==comp_two){
-            return 5;
+        else if(board[1]*board[5]*board[9]==125){
+            winner=2;
+            return -1;
         }
-        if(board[i]*board[i+1]*board[i+2]==opp_two){
-            return -5;
+        if(board[3]*board[5]*board[7]==27){
+            winner=1;
+            return 1;
         }
-    }
-
-    for(int i=1;i<=9; i=i++){
-        if(board[i]*board[i+3]*board[i+6]==comp_two){
-            return 5;
+        else if(board[3]*board[5]*board[7]==125){
+            winner=2;
+            return -1;
         }
-        if(board[i]*board[i+3]*board[i+6]==opp_two){
-            return -5;
-        }
-    }
-
-    if(board[1]*board[5]*board[9] == comp_two){
-        return 5;
-    }
-    if(board[3]*board[5]*board[7] == opp_two){
-        return -5
     }
     return 0;
 }
 
 
-int MiniMax(vector<int>board, int depth, int player){
-    if(dep==depth){
-        return evaluate(player);
+int MiniMax(vector<int>board,int depth,bool maxi){
+    int ans = updatewin(board);
+    if(d==depth){    
+        return ans;
     }
-    vector<vector<int>>next_move=move_gen();
-    if(next_move.size()==0){
-        return evaluate(player);
+    if (ans != 0){    
+        return ans;
     }
-    int mx=INT_MIN;
-    vector<int>best_config
-    for(int i=0; i<next_move.size();i++){
-        int config=MiniMax(next_move[i],depth+1, !player);
-        config=-config;
-        if(config>mx){
-            mx=config;
-            best_config=next_move[i];
+    int temp=1,curr=3;
+    if(!maxi) temp=-1,curr=5;
 
+    int bestScore = INT_MIN;
+    for(int i=1;i<=9;i++){
+        if (board[i] == 2){
+            board[i]=curr;
+            int score = temp*MiniMax(board,depth+1,!maxi);
+            board[i]=2;
+            bestScore = max(score, bestScore);
         }
     }
-    board=best_config;
-    return mx;
+    if(bestScore==INT_MIN)
+        return 0;
+    return temp*bestScore;
 }
 
-bool draw_cond(){
-    if(turn>9){
-        cout<<"Draw"<<endl;
-        exit(0);
+void MoveSelector(vector<int>& board,int &player){
+    int curr=3;
+    int bestMove;
+    int temp = INT_MIN;
+    for(int i=1;i<=9;i++){
+        if(board[i]==2){
+            board[i]=curr;
+            int value = MiniMax(board,0,0);
+            if(value>temp){
+                temp=value;
+                bestMove=i;
+            }
+            board[i]=2;
+        }
     }
+    player=2;
+    if(temp==INT_MIN)
+        return;
+    board[bestMove]=curr;
 }
 
+bool turnPossible(vector<int>board){
+    for(int i=1;i<=9;i++){
+        if(board[i]==2)
+            return 1;
+    }
+    return 0;
+}
 
 
 int main(){
+    vector<int>board(10,2);
     
     int choice;
 
     cout<<"Do You Want To Play?"<<endl;
     cout<<"1. Yes"<<endl;
     cout<<"2. No"<<endl;
+
+    cin>>choice;
+
+    int player;
+    if(choice==1)    player = 2;
+    else    player = 1;
     
-    return 0;
+    while(turnPossible(board)){
+        if(player==2){
+            while(1){
+                cout<<"Your move at position: "<<endl;
+                int pos;
+                cin>>pos;
+                if(board[pos]==2){
+                    board[pos]=5;
+                    break;
+                }
+                else
+                    cout<<"Position Already Occupied"<<endl; 
+            }
+            player=1;
+        }
+        else{
+            MoveSelector(board,player);
+        }
+        win=abs(updatewin(board));
+        display(board);
+        string winner_name;
+        if(winner==1){
+            winner_name="computer";
+        }
+        else{
+            winner_name="user";
+        }
+        if(win){
+            cout<<winner<<" wins "<<endl;
+            break;
+        }
+    }
+    if(!win && !turnPossible(board))
+        cout<<"Draw"<<endl;
 }
